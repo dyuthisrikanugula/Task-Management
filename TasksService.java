79fa84b6b81fbf46f17426b1354c1ef73731b7ca
dyuthisrikanugula <22bq1a1269@vvit.net>
2024-06-23 19:53:02 +0530

    TasksService.java

---
+++ TasksService.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import java.util.List;
+
+public interface TasksService {
+
+	public List<Tasks> getcompletedtasks();
+	
+	public void addcompletedtasks(Customers tasks);
+}
